7ece06a8b91e0d02460b3980d1a69929158c2f81
Agathe <gat.the.blues@gmail.com>
2026-04-22 17:27:01 +0200

    feat: implementation étape extraction et test

--- src/extract.py
+++ src/extract.py
@@ -1,4 +1,5 @@
 import requests
+
 from datetime import datetime
 from config import API_KEY, BASE_URL
 
@@ -6,20 +7,40 @@ from config import API_KEY, BASE_URL
 def extract_weather(city: str) -> dict:
     url = f"{BASE_URL}?q={city}&appid={API_KEY}&units=metric"
 
+    # Récupérer la réponse de la requête http GET sur le serveur de OpenWeatherMap
     response = requests.get(url)
+    # Lever une exception en cas d'échec de la requête
     response.raise_for_status()
 
+    # Parser la réponse json en dictionnaire python
     data = response.json()
+    weather_list = data.get("weather") or [{}] #après le 'or' : si 'weather' est vide, alors retourner une liste avec un dict vide
 
     return {
         "city": data.get("name"),
         "temperature": data.get("main", {}).get("temp"),
         "humidity": data.get("main", {}).get("humidity"),
-        "weather": data.get("weather", [{}])[0].get("main"),
-        "timestamp": datetime.utcnow().isoformat()
+        "weather": weather_list[0].get("description"),
+        "timestamp": data.get("dt"),
+        "datetime": datetime.utcfromtimestamp(data.get("dt")).isoformat()
     }
 
 
+
 if __name__ == "__main__":
-    result = extract_weather("Paris")
-    print(result)
+    cities = ["Paris", "London", "Berlin", "Madrid"]
+
+    results = []
+
+    for city in cities:
+        print(f"[INFO] Fetching data for {city}...")
+
+        try:
+            result = extract_weather(city)
+            results.append(result)
+        except Exception as e:
+            print(f"Erreur pour {city}: {e}")
+
+    print("\n### Résultats:\n")
+    for r in results:
+        print(r)
--- src/test_api.py
+++ src/test_api.py
@@ -1,4 +1,7 @@
 import requests
+import json
+
+from datetime import datetime
 from config import API_KEY, BASE_URL
 
 params = {
@@ -7,8 +10,28 @@ params = {
     "units": "metric"
 }
 
+#test réponse API
 response = requests.get(BASE_URL, params=params)
 
+print("## 1. test réponse API\n")
 print("Status code:", response.status_code)
-print("Response JSON:")
-print(response.json())
+print("\nResponse JSON:")
+print(response.json(), "\n")
+
+#test parsing JSON
+data = response.json()
+weather_list = data.get("weather") or [{}]
+
+print("## 2. test parsing JSON\n")
+print(json.dumps(data, indent=2), "\n")
+
+parsed = {
+        "city": data.get("name"),
+        "temperature": data.get("main", {}).get("temp"),
+        "humidity": data.get("main", {}).get("humidity"),
+        "weather": weather_list[0].get("description"),
+        "timestamp": data.get("dt"),
+        "datetime": datetime.utcfromtimestamp(data.get("dt")).isoformat()
+}
+
+print(" data parsées :\n", parsed)
